bc2b61c93023cbfa0ae41d5be63039bb5d0e512b
Ainur <ainurminu@mail.ru>
2022-07-22 15:41:57 +0300

    Version 1.2.0.1 Trying to deploy 6

--- speaking_club/asgi.py
+++ speaking_club/asgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 import os
 
+import django
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -16,6 +17,7 @@ from django.core.asgi import get_asgi_application
 import club.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'speaking_club.settings')
+django.setup()
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
