44d212880ed172d271c27f874f9eae7eb1f3bd97
Ainur <ainurminu@mail.ru>
2022-07-22 17:18:47 +0300

    Version 1.2.0.1 Trying to deploy 20

--- speaking_club/settings.py
+++ speaking_club/settings.py
@@ -81,7 +81,7 @@ CHANNEL_LAYERS = {
         # "BACKEND": "channels.layers.InMemoryChannelLayer",
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [(os.environ.get('REDIS_URL'), )],
+            "hosts": [(os.environ.get('REDIS_URL'))]
             # "hosts": [os.environ.get('REDIS_URL', 6379)],
             # "hosts": [(os.environ.get('REDIS_URL', 'redis://localhost:6379'), )],
         },
